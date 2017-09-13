4767f082ae8b3a7550dbd39e6927f028c1ffd5c9
lixiang12138 <zhao20130673@163.com>
2017-09-13 15:45:54 +0800

    test001

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,9 @@
 package com.company;
-
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("WTF?");
         System.out.println("Wow2");
         System.out.println("Woooooow2");
+        System.out.println("123");
     }
 }
