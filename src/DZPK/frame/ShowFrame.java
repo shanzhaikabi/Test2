0f41f5f754eaa3b83a8e4521c552f1689555f041
shanzhaikabi <shanzhaikabi@users.noreply.github.com>
2017-09-14 10:38:29 +0800

    emmm

--- src/DZPK/frame/ShowFrame.java
+++ src/DZPK/frame/ShowFrame.java
@@ -10,14 +10,14 @@ public class ShowFrame {
     public static void test(){
         JFrame frame = new JFrame("测试专用弹窗");
         frame.setSize(800,600);
-        JPanel panel = new JPanel();
-        JTextArea textArea = new JTextArea();
-
-        panel.setLayout(new GridLayout());
-        textArea.setText("test");
-        //当TextArea里的内容过长时生成滚动条
-        panel.add(new JScrollPane(textArea));
-        frame.add(panel);
+        Dimension displaySize = Toolkit.getDefaultToolkit().getScreenSize(); // 获得显示器大小对象
+        Dimension frameSize = frame.getSize();             // 获得窗口大小对象
+        if (frameSize.width > displaySize.width)
+            frameSize.width = displaySize.width;           // 窗口的宽度不能大于显示器的宽度
+        if (frameSize.height > displaySize.height)
+            frameSize.height = displaySize.height;          // 窗口的高度不能大于显示器的高度
+        frame.setLocation((displaySize.width - frameSize.width) / 2,
+                (displaySize.height - frameSize.height) / 2); // 设置窗口居中显示器显示
 
         frame.setVisible(true);
     }
