d47d92446f0410a247a58689100ad724125da00c
lixiang12138 <zhao20130673@163.com>
2018-01-02 14:20:12 +0800

    1/2 游戏界面v1.1 增加了每盘的结算界面

--- src/com/company/DZPK/frame/game_frame.java
+++ src/com/company/DZPK/frame/game_frame.java
@@ -14,6 +14,7 @@ import javax.swing.DropMode;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import java.awt.Color;
+import javax.swing.SwingConstants;
 
 public class game_frame {
 
@@ -53,12 +54,170 @@ public class game_frame {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
-		JLabel player1_name_label = new JLabel("\u73A9\u5BB61");
+		JPanel ending_panel = new JPanel();
+		ending_panel.setBounds(128, 80, 778, 560);
+		frame.getContentPane().add(ending_panel);
+		ending_panel.setLayout(null);
+		
+		JLabel ed_nuts_label = new JLabel("\u540C \u82B1 Dark \u987A");
+		ed_nuts_label.setHorizontalAlignment(SwingConstants.CENTER);
+		ed_nuts_label.setFont(new Font("宋体", Font.PLAIN, 48));
+		ed_nuts_label.setBounds(14, 13, 750, 60);
+		ending_panel.add(ed_nuts_label);
+		
+		JLabel ed_winner_label = new JLabel("\u53F8\u673A\u59EC");
+		ed_winner_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_winner_label.setBounds(14, 86, 120, 40);
+		ending_panel.add(ed_winner_label);
+		
+		JLabel ed_board_label = new JLabel("\u5E95\u724C");
+		ed_board_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_board_label.setBounds(14, 139, 120, 40);
+		ending_panel.add(ed_board_label);
+		
+		JLabel ed_nuts_detail_label = new JLabel("\u9ED1\u6843A \u9ED1\u6843K \u9ED1\u6843Q \u9ED1\u6843J \u9ED1\u684310");
+		ed_nuts_detail_label.setHorizontalAlignment(SwingConstants.CENTER);
+		ed_nuts_detail_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_nuts_detail_label.setBounds(14, 86, 750, 40);
+		ending_panel.add(ed_nuts_detail_label);
+		
+		JLabel ed_board_detail_label = new JLabel("\u9ED1\u6843K \u9ED1\u6843Q \u9ED1\u68439 \u672A\u77E5 \u672A\u77E5");
+		ed_board_detail_label.setHorizontalAlignment(SwingConstants.CENTER);
+		ed_board_detail_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_board_detail_label.setBounds(14, 139, 750, 40);
+		ending_panel.add(ed_board_detail_label);
+		
+		JLabel ed_player1_name_label = new JLabel("\u53F8\u673A\u59EC");
+		ed_player1_name_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player1_name_label.setBounds(14, 192, 375, 40);
+		ending_panel.add(ed_player1_name_label);
+		
+		JLabel ed_player1_money_label = new JLabel("25000");
+		ed_player1_money_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player1_money_label.setBounds(389, 192, 189, 40);
+		ending_panel.add(ed_player1_money_label);
+		
+		JLabel ed_player1_delta_label = new JLabel("+32000");
+		ed_player1_delta_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player1_delta_label.setBounds(575, 192, 189, 40);
+		ending_panel.add(ed_player1_delta_label);
+		
+		JLabel ed_player2_name_label = new JLabel("\u73A9\u5BB62");
+		ed_player2_name_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player2_name_label.setBounds(14, 245, 375, 40);
+		ending_panel.add(ed_player2_name_label);
+		
+		JLabel ed_player2_money_label = new JLabel("25000");
+		ed_player2_money_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player2_money_label.setBounds(389, 245, 189, 40);
+		ending_panel.add(ed_player2_money_label);
+		
+		JLabel ed_player2_delta_label = new JLabel("-16000");
+		ed_player2_delta_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player2_delta_label.setBounds(575, 245, 189, 40);
+		ending_panel.add(ed_player2_delta_label);
+		
+		JLabel ed_player3_name_label = new JLabel("\u73A9\u5BB63");
+		ed_player3_name_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player3_name_label.setBounds(14, 298, 375, 40);
+		ending_panel.add(ed_player3_name_label);
+		
+		JLabel ed_player3_money_label = new JLabel("25000");
+		ed_player3_money_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player3_money_label.setBounds(389, 298, 189, 40);
+		ending_panel.add(ed_player3_money_label);
+		
+		JLabel ed_player3_delta_label = new JLabel("-16000");
+		ed_player3_delta_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player3_delta_label.setBounds(575, 298, 189, 40);
+		ending_panel.add(ed_player3_delta_label);
+		
+		JLabel ed_player4_name_label = new JLabel("\u73A9\u5BB64");
+		ed_player4_name_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player4_name_label.setBounds(14, 351, 375, 40);
+		ending_panel.add(ed_player4_name_label);
+		
+		JLabel ed_player4_money_label = new JLabel("25000");
+		ed_player4_money_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player4_money_label.setBounds(389, 351, 189, 40);
+		ending_panel.add(ed_player4_money_label);
+		
+		JLabel ed_player4_delta_label = new JLabel("0");
+		ed_player4_delta_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player4_delta_label.setBounds(575, 351, 189, 40);
+		ending_panel.add(ed_player4_delta_label);
+		
+		JLabel ed_player5_name_label = new JLabel("\u73A9\u5BB65");
+		ed_player5_name_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player5_name_label.setBounds(14, 404, 375, 40);
+		ending_panel.add(ed_player5_name_label);
+		
+		JLabel ed_player5_money_label = new JLabel("25000");
+		ed_player5_money_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player5_money_label.setBounds(389, 404, 189, 40);
+		ending_panel.add(ed_player5_money_label);
+		
+		JLabel ed_player5_delta_label = new JLabel("0");
+		ed_player5_delta_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player5_delta_label.setBounds(575, 404, 189, 40);
+		ending_panel.add(ed_player5_delta_label);
+		
+		JLabel ed_player6_name_label = new JLabel("\u73A9\u5BB66");
+		ed_player6_name_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player6_name_label.setBounds(14, 457, 375, 40);
+		ending_panel.add(ed_player6_name_label);
+		
+		JLabel ed_player6_money_label = new JLabel("25000");
+		ed_player6_money_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player6_money_label.setBounds(389, 457, 189, 40);
+		ending_panel.add(ed_player6_money_label);
+		
+		JLabel ed_player6_delta_label = new JLabel("0");
+		ed_player6_delta_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player6_delta_label.setBounds(575, 457, 189, 40);
+		ending_panel.add(ed_player6_delta_label);
+		
+		JLabel ed_player1_hole_label = new JLabel("\u9ED1\u6843J \u9ED1\u684310");
+		ed_player1_hole_label.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player1_hole_label.setBounds(120, 192, 189, 40);
+		ending_panel.add(ed_player1_hole_label);
+		
+		JLabel ed_player2_hole_hand = new JLabel("\u9ED1\u684310 \u9ED1\u68437");
+		ed_player2_hole_hand.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player2_hole_hand.setBounds(120, 245, 189, 40);
+		ending_panel.add(ed_player2_hole_hand);
+		
+		JLabel ed_player3_hole_hand = new JLabel("\u7EA2\u6843J \u9ED1\u684310");
+		ed_player3_hole_hand.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player3_hole_hand.setBounds(120, 298, 189, 40);
+		ending_panel.add(ed_player3_hole_hand);
+		
+		JLabel ed_player4_hole_hand = new JLabel("\u5F03\u724C");
+		ed_player4_hole_hand.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player4_hole_hand.setBounds(120, 351, 189, 40);
+		ending_panel.add(ed_player4_hole_hand);
+		
+		JLabel ed_player5_hole_hand = new JLabel("\u5F03\u724C");
+		ed_player5_hole_hand.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player5_hole_hand.setBounds(120, 404, 189, 40);
+		ending_panel.add(ed_player5_hole_hand);
+		
+		JLabel ed_player6_hole_hand = new JLabel("\u5F03\u724C");
+		ed_player6_hole_hand.setFont(new Font("宋体", Font.PLAIN, 22));
+		ed_player6_hole_hand.setBounds(120, 457, 189, 40);
+		ending_panel.add(ed_player6_hole_hand);
+		
+		JButton continue_button = new JButton("Continue");
+		continue_button.setFont(new Font("宋体", Font.PLAIN, 22));
+		continue_button.setBounds(309, 510, 160, 40);
+		ending_panel.add(continue_button);
+		
+		JLabel player1_name_label = new JLabel("\u53F8\u673A\u59EC");
 		player1_name_label.setFont(new Font("宋体", Font.PLAIN, 22));
 		player1_name_label.setBounds(27, 29, 120, 40);
 		frame.getContentPane().add(player1_name_label);
 		
-		JLabel palyer1_label = new JLabel("\u5F53\u524D\u72B6\u6001");
+		JLabel palyer1_label = new JLabel("ALL-IN");
 		palyer1_label.setFont(new Font("宋体", Font.PLAIN, 18));
 		palyer1_label.setBounds(27, 82, 120, 40);
 		frame.getContentPane().add(palyer1_label);
