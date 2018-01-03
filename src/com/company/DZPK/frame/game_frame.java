d0a033f35502d9fe1abeed507e998c4d1bf7fe5f
lixiang12138 <zhao20130673@163.com>
2018-01-03 20:42:00 +0800

    1/3 20：41 第三次解决了提出问题的李霄

--- src/com/company/DZPK/frame/game_frame.java
+++ src/com/company/DZPK/frame/game_frame.java
@@ -83,6 +83,22 @@ public class game_frame {
 	public static String unknown = "\u672a\u77e5";
 	public static String hole_string ="\u60A8\u7684\u624B\u724C\uFF1A";
 	public static String board_string ="\u5E95\u724C\u72B6\u6001\uFF1A";
+	public static String raise_string="\u52A0\u6CE8";
+	public static String call_string="\u8DDF\u6CE8";
+	public static String fold_string="\u5F03\u724C";
+	public static String straight_flush_string="\u540C\u82B1\u987A";
+	public static String four_of_a_kind_string="\u56DB\u6761";
+	public static String full_house_string="\u846B\u82A6";
+	public static String flush_string="\u540C\u82B1";
+	public static String straight_string="\u987A\u5B50";
+	public static String three_of_a_kind_string="\u4E09\u6761";
+	public static String two_pair_string="\u4E24\u5BF9";
+	public static String one_pair_string="\u4E00\u5BF9";
+	public static String big_blind_string="\u5927\u76F2\u6CE8";
+	public static String high_card_string="\u9AD8\u724C";
+	public static String small_blind_string="\u5C0F\u76F2\u6CE8";
+	public static String raise_high_string="\u7B79\u7801\u4E0D\u8DB3\uFF01";
+	public static String raise_low_string="\u8DDF\u6CE8\u91D1\u989D\u4E0D\u8DB3\uFF01";
 
 	/**
 	 * Launch the application.
@@ -129,6 +145,10 @@ public class game_frame {
 		frame.setBounds(100, 100, 1052, 825);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
+
+		JLabel lblNewLabel = new JLabel("\u5927\u76F2\u6CE8");
+		lblNewLabel.setBounds(109, 43, 72, 18);
+		frame.getContentPane().add(lblNewLabel);
 		
 		JPanel ending_panel = new JPanel();
 		ending_panel.setBounds(128, 80, 778, 560);
